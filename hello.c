38f2c83354d21806ec4b16e76c6501fcb9880d74
Kamil-krasniewski <k.krasniewski.480@studms.ug.edu.pl>
2025-11-10 16:15:00 +0100

    Nowa linia do hello.c

--- hello.c
+++ hello.c
@@ -1,1 +1,6 @@
-#include <stdio.h> int main(){ printf("Hello World!\\n"); printf("Ala ma kota\\n");}
+#include <stdio.h> 
+int main(){ 
+  printf("Hello World!\\n"); 
+  printf("Ala ma kota\\n");
+  printf("Kota ma szefa\\n");
+}
